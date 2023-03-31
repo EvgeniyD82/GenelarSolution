69baa0729189d477be78252aee8b0ed8bc4758bd
Evgeny Dumanov <dum_evg@mail.ru>
2023-03-31 14:39:20 +0300

    Add additional line to console

--- GitTest1/Program.cs
+++ GitTest1/Program.cs
@@ -10,6 +10,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
             Console.WriteLine("What's your name?");
             var name = Console.ReadLine();
             Console.WriteLine("Hello,"+name);
+            Console.WriteLine("Hello," + name);
         }
     }
 }
