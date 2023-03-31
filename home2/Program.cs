f6138f7481ca4f2282d3cc27097428c5be8da6b6
Evgeny Dumanov <dum_evg@mail.ru>
2023-03-31 15:10:58 +0300

    add project

---
+++ home2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Security;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello," + name);
+        }
+    }
+}
